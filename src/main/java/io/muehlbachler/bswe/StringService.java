de545e841e622a9be51e6c15e8145c844c2c15ba
Kinteshi <146526333+Kinteshi1@users.noreply.github.com>
2024-10-09 19:27:53 +0200

    Update StringService.java

--- src/main/java/io/muehlbachler/bswe/StringService.java
+++ src/main/java/io/muehlbachler/bswe/StringService.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Service;
 
 // FIXME: add javadoc
 /**
-    Services Strings, smile
+    Services Class for String Manipulation
 */
 @Service
 public class StringService {
     // FIXME: add javadoc
     /**
         Returns a string in UpperCase Letters
+        @param String to be converted
+        @return the String given in UpperCase letters
     */
     public String toUpperCase(final String string) {
         // FIXME: implement
